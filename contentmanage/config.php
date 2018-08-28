706553f35ad41fc0209cd8badcb2dd1c0437d971
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-08-28 20:40:40 -0300

    #

---
+++ contentmanage/config.php
@@ -0,0 +1,5 @@
+<?php
+define ( "ROOT", dirname ( __FILE__ ) . DIRECTORY_SEPARATOR );
+define ( "DAO", ROOT . 'dao' . DIRECTORY_SEPARATOR );
+define ( "MODEL", ROOT . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR );
+define ( "UPLOAD", ROOT . 'upload' . DIRECTORY_SEPARATOR );
