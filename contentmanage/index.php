1aadc011b96e2419e5ba1d146f4a91f1dcd6b199
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-09-18 09:23:50 -0300

    #

--- contentmanage/index.php
+++ contentmanage/index.php
@@ -71,11 +71,11 @@ if (isset($_GET['promo'])) { // show promotions
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 
-	<!-- BEGIN JIVOSITE CODE {literal} -->
-	<script type='text/javascript'>
-	(function(){ var widget_id = 'Pzg6FEUP4j';var d=document;var w=window;function l(){
-	var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
-	<!-- {/literal} END JIVOSITE CODE -->
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'RdML1V2hqd';var d=document;var w=window;function l(){
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
 
 </head>
 
